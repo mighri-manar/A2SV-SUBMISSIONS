7beb543f79d9a28221085f4133d2f3820701fb98
Manar Mighri <144473914+mighri-manar@users.noreply.github.com>
2025-03-03 17:23:37 +0100

    Created 'Toeplitz matrix' by A2SV Hub

---
+++ 03-Mar-2025/Toeplitz matrix 289806.py
@@ -0,0 +1,9 @@
+# Problem: Toeplitz matrix - https://leetcode.com/problems/toeplitz-matrix/description/
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i-1][j-1] != matrix[i][j]:
+                    return False
+        return True
